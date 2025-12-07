08dfe6306dc07d3b56cf031a40a9d8e01845b368
Angel Ysmael <angelysmael7@gmail.com>
2025-12-07 17:14:34 -0500

    gettotalvalue method

--- script.js
+++ script.js
@@ -5,3 +5,16 @@ class Product {
     this.quantity = quantity;
   }
 }
+// Product class: basic product information
+class Product {
+  constructor(name, price, quantity) {
+    this.name = name;
+    this.price = price;
+    this.quantity = quantity;
+  }
+
+  // Returns total value of this product in stock
+  getTotalValue() {
+    return this.price * this.quantity;
+  }
+}
